fa8563866433ae1f60dd851275b238c8759004bb
HacKids Education <65713501+HacKids-Edu@users.noreply.github.com>
2020-09-18 16:18:20 -0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-let baseline = input.magneticForce(Dimension.Strength)
+let intensidade = input.magneticForce(Dimension.Strength)
 basic.clearScreen()
 basic.forever(function () {
-    if (Math.abs(input.magneticForce(Dimension.Strength) - baseline) > 100) {
+    if (Math.abs(input.magneticForce(Dimension.Strength) - intensidade) > 100) {
         basic.showIcon(IconNames.Skull)
     } else {
         basic.clearScreen()
